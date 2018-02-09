535843f5836d9f3a226c6ef65550df7fdb7f1b37
Emily Jirles <emily.jirles@gmail.com>
2018-02-09 15:21:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -60,6 +60,6 @@ function placeOrder(cardNumber) {
     console.log("Sorry, we don't have a credit card on file for you.")
   } else {
     cart = []
-    return `Your total cost is $${total()}, which will be charged to the cart ${cardNumber}.`
+    console.log(`Your total cost is $${total()}, which will be charged to the cart ${cardNumber}.`)
   }
 }
