7d8ca7a4d7f91173412903435d05be9b7fd46348
Emily Jirles <emily.jirles@gmail.com>
2018-02-09 14:22:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,18 +21,32 @@ function viewCart() {
   } else if (cart.length === 1){
     return `In your cart, you have ${Object.key(cart[0])} at $${cart[0][Object.key(cart[0])]}.`
   } else if (cart.length === 2){
-    return `In your cart, you have ${Object.key(cart[0])} at $${cart[0][Object.key(cart[0])]} and ${Object.key(cart[1])} at $${}.`
+    return `In your cart, you have ${Object.key(cart[0])} at $${cart[0][Object.key(cart[0])]} and ${Object.key(cart[1])} at $${cart[1][Object.key(cart[1])]}.`
   } else {
-    return
+    var message = 'In your cart, you have'
+    for (var i = 0; i < cart.length; i++) {
+      if (i === cart.length - 1) {
+        message += ` and ${Object.key(cart[i])} at $${cart[i][Object.key(cart[i])]}.`
+      } else {
+        message += ` ${Object.key(cart[i])} at $${cart[i][Object.key(cart[i])]},`
+      }
+    }
+    return message
   }
 }
 
 function total() {
-  // write your code here
+  var total = 0
+  for (var i = 0; i < cart.length; i++) {
+    total += cart[i][Object.key(cart[i])]
+  }
+  return total
 }
 
 function removeFromCart(item) {
-  // write your code here
+  if (item not in cart) {
+    return "That item is not in your cart."
+  }
 }
 
 function placeOrder(cardNumber) {
